da17a8361c1c66812e00fae8defeb30bc2998c58
Dipankar Roy <78965407+dipankar-cmd@users.noreply.github.com>
2022-10-31 17:36:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# #madlibs
+# # string concatenation
+animal = input("Animal: ")
+cartoon_char = input("Cartoon Character: ")
+famous_person = input("A famous person: ")
+
+madlib = f"I saw a {animal} in my dream and it was eating {cartoon_char}. Also the face of the {animal} was an exact " \
+         f"replica of {famous_person} "
+
+print(madlib)
